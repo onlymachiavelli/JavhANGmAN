cd2dd2f34b77bb670974ac2da4be2e901bd9f5f3
onlymachiavelli <tmakaveli643@gmail.com>
2021-08-03 16:05:57 +0100

    Creating the file

---
+++ Game.java
@@ -0,0 +1,11 @@
+public class Game {
+    // Just to make the syntax easier !
+
+    public static void print(Object obj) {
+        System.out.println(obj);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
