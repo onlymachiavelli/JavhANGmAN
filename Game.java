8c4fe46eea8183c6df5e17ecd2b55f5f2f7a3b0b
onlymachiavelli <tmakaveli643@gmail.com>
2021-08-03 16:46:30 +0100

    Setting some funcs

--- Game.java
+++ Game.java
@@ -1,3 +1,6 @@
+import java.io.Reader;
+import java.util.Scanner;
+
 public class Game {
     // Just to make the syntax easier !
 
@@ -5,7 +8,24 @@ public class Game {
         System.out.println(obj);
     }
 
+    public static void input(Object obj) {
+        Scanner Reader = new Scanner(System.in);
+        obj = Reader.nextLine();
+    }
+
+    public boolean Check(String word, char guess) {
+        int i;
+        // linear search for the char in the word
+        for (i = 0; i < word.length(); i++) {
+            if (guess == word.charAt(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
+        String word = "hello";
 
     }
 }
